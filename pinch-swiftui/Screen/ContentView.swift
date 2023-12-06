8c0ca15e23c840976ad107f8b59d5b8a0cb4ba1d
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-06 23:45:56 +0530

    feature: added Long Press gesture + integrated info panel

--- pinch-swiftui/Screen/ContentView.swift
+++ pinch-swiftui/Screen/ContentView.swift
@@ -27,6 +27,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
                 // MARK: PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -70,6 +71,13 @@ struct ContentView: View {
                     isAnimating = true
                 }
             })
+            // MARK: INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                , alignment: .top
+            )
         } //: NAVIGATION
         .navigationViewStyle(.stack)
     }
