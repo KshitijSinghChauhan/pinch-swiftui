e05708db37208f425b9c57c9aab7cdc3b08d24f4
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-07 00:03:51 +0530

    feature: added zoom in, zoom out and reset buttons with functionalities

---
+++ pinch-swiftui/View/ControlImageView.swift
@@ -0,0 +1,25 @@
+//
+//  ControlImageView.swift
+//  pinch-swiftui
+//
+//  Created by Kshitij Singh Chauhan on 06/12/23.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
