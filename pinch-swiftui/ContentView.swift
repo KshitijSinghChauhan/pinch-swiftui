30997b33de60e45fb5bc366bb563202a17288d61
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-06 16:47:11 +0530

    initial commit

---
+++ pinch-swiftui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  pinch-swiftui
+//
+//  Created by Kshitij Singh Chauhan on 06/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pinch-swiftui/pinch_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinch_swiftuiApp.swift
+//  pinch-swiftui
+//
+//  Created by Kshitij Singh Chauhan on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct pinch_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
