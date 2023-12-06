f4d26e3af411c3af9e3b0802edf2ed0bea80066c
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-06 17:42:05 +0530

    feature: added image to content view

--- pinch-swiftui/Screen/ContentView.swift
+++ pinch-swiftui/Screen/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: PROPERTY
+    
+    @State private var isAnimating: Bool = false
+    
+    // MARK: FUNCTION
+    
+    // MARK: CONTENT
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack {
+                // MARK: PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+            } //: ZSTACK
+            .navigationTitle("Pinch and Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            })
+        } //: NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
 
